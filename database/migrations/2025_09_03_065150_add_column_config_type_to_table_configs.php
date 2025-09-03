af0e1b85d21e4de9e59bf8eaf4b3ed87555dcf47
maiduong <duongmdph40323@gmail.com>
2025-09-03 14:00:51 +0700

    Add Column Config Type To Table Configs

---
+++ database/migrations/2025_09_03_065150_add_column_config_type_to_table_configs.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('configs', function (Blueprint $table) {
+            $table->smallInteger('config_type')->nullable()->after('config_key')->comment('Loại cấu hình 1: Type image, 2: Type string');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('configs', function (Blueprint $table) {
+            $table->dropColumn('config_type');
+        });
+    }
+};
