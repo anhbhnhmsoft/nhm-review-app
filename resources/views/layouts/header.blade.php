eb8a997e3159751490284c1f7420e4c8b9f5e581
truonghocdot05 <truonghocdot05@gmail.com>
2025-09-10 17:52:17 +0700

    Truonghd - Fix BUg

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,11 +6,11 @@
                 window.location.href = '{{ route('frontend.search-store') }}?sortBy=distance';
                 return;
             }
-            
+
             const location = await $GeoPlugin.getCurrentLocation();
             $GeoPlugin.storeLocation(location.lat, location.lng);
             window.location.href = '{{ route('frontend.search-store') }}?sortBy=distance';
-            
+
         } catch (error) {
             alert('Lỗi: ' + error.message);
         }
@@ -33,7 +33,9 @@
                     {{-- Tìm xung quanh --}}
                     <button @click="findNearby()" class="inline-flex items-center gap-2 group">
                         <i class="fa-solid fa-location-dot text-blue-500 text-xl group-hover:animate-bounce"></i>
-                        <span class="uppercase font-bold text-base transition-colors duration-300 group-hover:text-blue-500">Tìm xung quanh</span>
+                        <span
+                            class="uppercase font-bold text-base transition-colors duration-300 group-hover:text-blue-500">Tìm
+                            xung quanh</span>
                     </button>
                     {{-- Thông báo --}}
                     {{-- <a href="#" class="inline-flex items-center gap-2 group">
@@ -42,7 +44,8 @@
                     </a> --}}
                 </div>
                 <div class="inline-flex items-center gap-2">
-                    <a href="{{ route('frontend.search-store') }}" class="btn btn-primary-green text-white text-base font-medium rounded-lg">
+                    <a href="{{ route('frontend.search-store') }}"
+                        class="btn btn-primary-green text-white text-base font-medium rounded-lg">
                         Viết Review
                     </a>
                     @if (auth('web')->check())
@@ -62,7 +65,7 @@
                                     </a>
                                 </li>
                                 <li><a href="{{ route('frontend.profile') }}#saved">Địa điểm đã lưu</a></li>
-                                <li><a href="{{ route('frontend.logout') }}">Đăng xuất</a></li>
+
                             </ul>
                         </div>
                     @else
@@ -108,13 +111,7 @@
                                 </div>
 
                                 {{-- Nút đăng xuất --}}
-                                <form method="GET" action="{{ route('frontend.logout') }}">
-                                    @csrf
-                                    <button type="submit"
-                                        class="ml-3 px-3 py-1 text-sm font-medium text-white bg-red-500 rounded-md hover:bg-red-600 transition">
-                                        Đăng xuất
-                                    </button>
-                                </form>
+
                             @else
                                 <a href="{{ route('frontend.login') }}" class="btn btn-primary-green">
                                     Đăng nhập
@@ -138,12 +135,13 @@
                         <div class="h-[calc(100vh-57px)] overflow-y-auto p-4">
                             <nav class="flex flex-col gap-4 py-4">
 
-                                <a href="{{ route('frontend.profile') }}#review" class="text-base font-bold ">Đánh giá</a>
+                                <a href="{{ route('frontend.profile') }}#review" class="text-base font-bold ">Đánh
+                                    giá</a>
 
                                 <a href="{{ route('frontend.profile') }}#saved" class="text-base font-bold ">Đã lưu</a>
 
-                                <button @click="findNearby()" class="text-base font-bold text-left flex items-center gap-2">
-                                    <i class="fa-solid fa-location-dot text-blue-500"></i>
+                                <button @click="findNearby()"
+                                    class="text-base font-bold text-left flex items-center gap-2">
                                     Tìm xung quanh
                                 </button>
                                 <a href="#" class="text-base font-bold ">
@@ -161,6 +159,13 @@
                                 <a href="{{ route('frontend.news') }}" class="text-base font-bold ">
                                     Tin tức & Cẩm nang
                                 </a>
+                                <form method="GET" action="{{ route('frontend.logout') }}">
+                                    @csrf
+                                    <button type="submit"
+                                        class="px-3 py-1 text-sm font-medium text-white bg-red-500 rounded-md hover:bg-red-600 transition">
+                                        Đăng xuất
+                                    </button>
+                                </form>
                             </nav>
                         </div>
                     </aside>
